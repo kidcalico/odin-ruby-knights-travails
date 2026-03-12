e62a7431bb75252aad4bc5d2059bc8af245e9a28
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-03-12 14:54:29 -0700

    Update rspec test
    Testing still isn't finished for this project, but since I ended up not
    following TDD (still learning some fundamental concepts), I might wait
    and just apply it to the Chess project instead.

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -2,32 +2,32 @@ require_relative '../lib/knight'
 
 describe Knight do
   subject(:knight) { described_class.new([0, 0]) }
-  describe '#filter_moves' do
-    context 'given an array of all computed moves from a given position' do
-      it 'rejects all sub-arrays containing values less than 0' do
-        array = [[1, 2], [-1, 2], [2, 1], [2, -1], [-2, 1], [-2, -1], [1, -2], [-1, -2]]
+  describe '#possible_moves' do
+    context 'given a position' do
+      it 'returns all legal moves from a corner position' do
+        position = [0, 0]
         expected = [[1, 2], [2, 1]]
-        result = knight.filter_moves(array)
+        result = knight.possible_moves(position)
         expect(result).to eq(expected)
       end
 
-      it 'rejects all sub-arrays containing values more than 7' do
-        array = [[8, 9], [6, 9], [9, 8], [9, 6], [5, 8], [5, 6], [8, 5], [6, 5]]
-        expected = [[5, 6], [6, 5]]
-        result = knight.filter_moves(array)
+      it 'returns all legal moves from a side position' do
+        position = [7, 3]
+        expected = [[6, 5], [6, 1], [5, 4], [5, 2]]
+        result = knight.possible_moves(position)
         expect(result).to eq(expected)
       end
 
-      it 'returns an array containing sub-arrays all with values 0..7 (inclusive)' do
-        array = [[5, 6], [3, 6], [6, 5], [6, 3], [2, 5], [2, 3], [5, 2], [3, 2]]
-        expected = [[5, 6], [3, 6], [6, 5], [6, 3], [2, 5], [2, 3], [5, 2], [3, 2]]
-        result = knight.filter_moves(array)
+      it 'returns all legal moves from a central position' do
+        position = [4, 4]
+        expected = [[5, 6], [3, 6], [5, 2], [3, 2], [6, 5], [6, 3], [2, 5], [2, 3]]
+        result = knight.filter_moves(position)
         expect(result).to eq(expected)
       end
     end
   end
 
-  describe '#knight_moves'
+  describe '#knight_moves' do
     context 'given a position and a target' do
       xit 'returns an array of steps to get from the position to the target' do
       end
