efc9e3b8332e74cbd7ee063e141af93cb4d77154
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-03-12 14:36:07 -0700

    Refactor knight_moves, rename and refactor bfs
    Refactored knight_moves, using the length of @steps so that I could
    utilize a more simplified queue. Also renamed bfs to determine path,
    which now only returns a hash of all parents and children, which is used
    to make a list of the steps.

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,50 +1,47 @@
 class Knight
   def initialize(position = [0, 0], target = [7, 7])
     @parents = {}
+    @steps = []
     knight_moves(position, target)
   end
 
   def knight_moves(position, target)
+    determine_path(position, target)
+    define_steps(position, target)
     print "Initial location: #{position}\nTarget location: #{target}\n"
-    search = bfs(position, target)
-    puts "Congratulations! You made it in #{search[1]} moves! Here's your path:"
-    define_steps(position, target).each { |step| print "#{step}\n" }
+    puts "Congratulations! You made it in #{@steps.length - 1} moves! Here's your path:"
+    @steps.each { |step| print "#{step}\n" }
   end
 
   def define_steps(position, target)
-    steps = [target]
+    @steps << target
     last_step = @parents[target]
 
-    until steps.include?(position)
-      steps.unshift(last_step)
+    until @steps.include?(position)
+      @steps.unshift(last_step)
       last_step = @parents[last_step]
     end
-    steps
   end
 
-  def bfs(position, target)
-    queue = [[position, 0, nil]]
+  def determine_path(position, target)
+    queue = [position]
 
     until queue.empty?
       current_node = queue.shift
-      return current_node if current_node[0] == target
+      return @parents if current_node == target
 
-      possible_moves(current_node[0]).each do |move|
+      possible_moves(current_node).each do |move|
         next if @parents.include?(move)
 
-        @parents[move] = current_node[0]
-        queue << [move, current_node[1] + 1, current_node[0]]
+        @parents[move] = current_node
+        queue << move
       end
     end
   end
 
   def possible_moves(position)
     all_moves = all_moves(position)
-    filter_moves(all_moves)
-  end
-
-  def filter_moves(moves)
-    moves = moves.reject { |move| move.any? { |v| v < 0 || v > 7 } }
+    moves = all_moves.reject { |move| move.any? { |v| v < 0 || v > 7 } }
   end
 
   def all_moves(position)
--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'rubocop'
 require 'pry-byebug'
 require_relative 'lib/knight'
 
-Knight.new([4, 4], [7, 7])
+test = Knight.new([4, 4], [7, 7])
 print "\n"
 Knight.new([6, 6], [7, 7])
 print "\n"
